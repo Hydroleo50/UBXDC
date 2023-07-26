21fbf47b2da5a67764b5cf55dea52feb5b05a8a1
Hydroleo50 <104764806+Hydroleo50@users.noreply.github.com>
2023-07-26 21:08:06 +0200

    no modern switch case allowed >:(
    blame the IDE. now compatible with OPENJDK-11-JDK-HEADLESS, woo hoo

--- binxdcon.java
+++ binxdcon.java
@@ -13,23 +13,28 @@ public class binxdcon {
         System.out.print("Conversion type:");
         int d = scanner.nextInt();
         switch (d) {
-            case 1 -> {
+            case 1: {
                 System.out.println("(I am not done implementing multiple lines yet, so)");
                 System.out.print("Type your String of 'xd's now, all into one line:");
                 System.out.println(xdtoBinary());
+                break;
             }
-            case 2 -> {
+            case 2: {
                 System.out.println("(I am not done implementing multiple lines yet, so)");
                 System.out.print("Type your binary now, in one line:");
                 System.out.println(binarytoXd());
+                break;
             }
-            case 3 ->
+            case 3:
                 //System.out.print("Type your String now:");
                     System.out.println("NOT IMPLEMENTED YET!!");
-            case 4 ->
+                    break;
+            case 4:
                 //System.out.print("Type your xd-binary now:");
                     System.out.println("NOT IMPLEMENTED YET!!");
-            default -> System.out.println("Shit broke");
+                    break;
+            default: System.out.println("Shit broke");
+            break;
         }
     }
 
