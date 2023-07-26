46caddc313f39d9a0662920a8d84d4864df685e9
Hydroleo50 <104764806+Hydroleo50@users.noreply.github.com>
2023-07-26 21:29:57 +0200

    input mismatch, usage
    imagine not listening to the prompt

--- binxdcon.java
+++ binxdcon.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class binxdcon {
@@ -11,31 +12,38 @@ public class binxdcon {
         System.out.println("To convert a String to xd, type 3");
         System.out.println("To convert xd to String, type 4");
         System.out.print("Conversion type:");
-        int d = scanner.nextInt();
-        switch (d) {
-            case 1: {
-                System.out.println("(I am not done implementing multiple lines yet, so)");
-                System.out.print("Type your String of 'xd's now, all into one line:");
-                System.out.println(xdtoBinary());
-                break;
+
+            try {
+                int d = scanner.nextInt();
+                switch (d) {
+                    case 1: {
+                        System.out.println("(I am not done implementing multiple lines yet, so)");
+                        System.out.print("Type your String of 'xd's now, all into one line:");
+                        System.out.println(xdtoBinary());
+                        break;
+                    }
+                    case 2: {
+                        System.out.println("(I am not done implementing multiple lines yet, so)");
+                        System.out.print("Type your binary now, in one line:");
+                        System.out.println(binarytoXd());
+                        break;
+                    }
+                    case 3:
+                        //System.out.print("Type your String now:");
+                        System.out.println("NOT IMPLEMENTED YET!!");
+                        break;
+                    case 4:
+                        //System.out.print("Type your xd-binary now:");
+                        System.out.println("NOT IMPLEMENTED YET!!");
+                        break;
+                    default:
+                        System.out.println("Shit broke");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Usage: just type the number 1, 2, 3 or 4 depending on what you need to do.");
             }
-            case 2: {
-                System.out.println("(I am not done implementing multiple lines yet, so)");
-                System.out.print("Type your binary now, in one line:");
-                System.out.println(binarytoXd());
-                break;
-            }
-            case 3:
-                //System.out.print("Type your String now:");
-                    System.out.println("NOT IMPLEMENTED YET!!");
-                    break;
-            case 4:
-                //System.out.print("Type your xd-binary now:");
-                    System.out.println("NOT IMPLEMENTED YET!!");
-                    break;
-            default: System.out.println("Shit broke");
-            break;
-        }
+
     }
 
     public static String xdtoBinary()
@@ -58,3 +66,6 @@ public class binxdcon {
         return s;
     }
 }
+// THERE ARE NO COMMENTS ; THE CODE IS THE COMMENT! !!!! !!
+// that's a joke. there are no comments because the finished tasks are trivial
+// there will be comments for more complex stuff, probably
